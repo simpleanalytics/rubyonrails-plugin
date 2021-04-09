274d1e8f0a12c1a5363d706cfc15243e17b4746d
Mike Rogers <me@mikerogers.io>
2021-04-09 19:07:57 +0100

    Removing reference to rake_tasks - I'll add them later

--- lib/simple_analytics_rails/railtie.rb
+++ lib/simple_analytics_rails/railtie.rb
@@ -2,8 +2,4 @@ class SimpleAnalyticsRails::Railtie < Rails::Railtie
   initializer "simple_analytics_rails.configure_rails_initialization" do |app|
     app.config.middleware.insert_after(ActionDispatch::DebugExceptions, SimpleAnalyticsRails::Middleware::JavascriptInjection)
   end
-
-  rake_tasks do
-    # load "tasks/simple_analytics_rails.tasks"
-  end
 end
