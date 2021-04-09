4ba216b7589afdae05b98035e7c659da14f15b93
Mike Rogers <me@mikerogers.io>
2021-04-09 20:44:48 +0100

    Removing unnessessary rails files

--- spec/example_app/spec/requests/middleware_spec.rb
+++ spec/example_app/spec/requests/middleware_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe "Middleware", type: :request do
-  it "see the injected Javascript Snippet" do
+RSpec.describe "Example App (Integration) - Middleware", type: :request do
+  it "sees the injected Javascript Snippet" do
     get root_path
-    expect(response.body).to include('https://scripts.simpleanalyticscdn.com/latest.js')
+    expect(response.body).to include('<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js')
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,6 @@ require File.expand_path("../../spec/example_app/config/environment", __FILE__)
 
 require "rspec/rails"
 
-RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
-end
+#RSpec.configure do |config|
+  #config.infer_spec_type_from_file_location!
+#end
