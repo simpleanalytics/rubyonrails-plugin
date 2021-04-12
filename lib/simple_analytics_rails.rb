f370e9baf02f2c1870ec5d945da91103ece957ca
Mike Rogers <me@mikerogers.io>
2021-04-12 11:58:43 +0100

    Adding top level documentation for Yard docs (#4)
    * Adding top level documentation for Yard docs
    
    * Thank you Typo CI
    
    * Thank you stardardrb

--- lib/simple_analytics_rails.rb
+++ lib/simple_analytics_rails.rb
@@ -4,6 +4,19 @@ require_relative "simple_analytics_rails/javascript_script"
 require_relative "simple_analytics_rails/middleware/javascript_injection"
 require_relative "simple_analytics_rails/railtie" if defined?(Rails)
 
+# This will automatically inject the `SimpleAnalyticsRails::Middleware::JavascriptInjection` middleware if you're using Ruby on Rails.
+#
+# Sample Usage for configuration:
+#
+#   # config/initializers/simple_analytics.rb
+#   SimpleAnalyticsRails.configure do |configuration|
+#     configuration.hostname = "example.com"
+#     configuration.mode = "hash"
+#     configuration.skip_dnt = false
+#     configuration.ignore_pages = "/search/*,/account/*,/vouchers"
+#     configuration.inject_javascript_to_head = Rails.env.production?
+#   end
+#
 module SimpleAnalyticsRails
   class << self
     def configure
