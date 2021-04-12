7e688e7da978f9db38a1c4e94ce63d3b19586bf9
Mike Rogers <me@mikerogers.io>
2021-04-12 11:53:03 +0100

    Thank you stardardrb

--- lib/simple_analytics_rails/railtie.rb
+++ lib/simple_analytics_rails/railtie.rb
@@ -1,4 +1,4 @@
-# This is the class which injects our Middleware into Rails apps.
+# This is the class which injects our Middleware into Rails apps.
 # If `Rails` isn't defined this class isn't included. If you're using other Rack apps you can include the Middleware classes manually.
 class SimpleAnalyticsRails::Railtie < Rails::Railtie
   initializer "simple_analytics_rails.configure_rails_initialization" do |app|
