f7af639cfa74cb88c7e54c978454ac719963f1fa
Mike Rogers <me@mikerogers.io>
2021-04-23 17:22:35 +0100

    Adding new settings missed in 0.1.0

--- lib/simple_analytics_rails/configuration.rb
+++ lib/simple_analytics_rails/configuration.rb
@@ -6,11 +6,16 @@ module SimpleAnalyticsRails
       @ignore_pages = ""
       @collect_dnt = false
       @enabled = true
+      @custom_domain = ""
+      @auto_collect = true
+      @sa_global = ""
     end
 
     attr_accessor :ignore_pages
     attr_accessor :hostname
     attr_accessor :mode
+    attr_accessor :sa_global
+
     attr_writer :collect_dnt
     def collect_dnt?
       @collect_dnt
@@ -19,6 +24,20 @@ module SimpleAnalyticsRails
     attr_writer :enabled
     def enabled?
       @enabled
+    end
+
+    attr_writer :custom_domain
+    def script_domain
+      @custom_domain.present? ? @custom_domain : "scripts.simpleanalyticscdn.com"
+    end
+
+    def noscript_domain
+      @custom_domain.present? ? @custom_domain : "queue.simpleanalyticscdn.com"
+    end
+
+    attr_writer :auto_collect
+    def auto_collect?
+      @auto_collect
     end
 
     def to_h
